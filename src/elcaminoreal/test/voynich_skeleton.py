b0d27d86c71482aed0ad83ca1b6e17ba4fcac94f
Moshe Zadka <zadka.moshe@gmail.com>
2017-12-02 16:28:52 -0800

    froop

--- src/elcaminoreal/test/voynich_skeleton.py
+++ src/elcaminoreal/test/voynich_skeleton.py
@@ -28,3 +28,25 @@ def secret_filename(dependencies, _maybedeps):
 def create(args, dependencies):
     print("writing key to", args.key_file, "and public data to",
           dependencies['secret_filename'])
+
+@COMMANDS.command(dependencies=['secret_filename'],
+                  parser=argparser(
+                      argument('--name', required=True),
+                      argument('--value', required=True),
+                         ),
+                 )
+def encrypt(args, dependencies):
+    print("adding to", dependencies['secret_filename'],
+          "name", args.name,
+          "value", args.value)
+
+@COMMANDS.command(dependencies=['secret_filename'],
+                  parser=argparser(
+                      argument('--key-file', required=True),
+                      argument('--directory', required=True),
+                         ),
+                 )
+def decrypt(args, dependencies):
+    print("decrypting", dependencies['secret_filename'],
+          "using", args.key_file,
+          "into", args.directory)
