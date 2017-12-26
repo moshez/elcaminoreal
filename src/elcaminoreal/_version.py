ee666fb8a96b8d14611a5fc9d44d4a8260559efe
Moshe Zadka <zadka.moshe@gmail.com>
2017-12-25 20:04:04 -0800

    17.12.1

--- src/elcaminoreal/_version.py
+++ src/elcaminoreal/_version.py
@@ -3,9 +3,9 @@ Provides elcaminoreal version information.
 """
 
 # This file is auto-generated! Do not edit!
-# Use `python -m incremental.update gather` to change this file.
+# Use `python -m incremental.update elcaminoreal` to change this file.
 
 from incremental import Version
 
-__version__ = Version('elcaminoreal', 17, 12, 0)
+__version__ = Version('elcaminoreal', 17, 12, 1)
 __all__ = ["__version__"]
